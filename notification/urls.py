ad89585865263867f7f06ef23599a7564825b5c5
Nishant Sapkota <snishant306@gmail.com>
2024-12-30 22:11:30 +0545

    chore: add route for notification

--- notification/urls.py
+++ notification/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UpdateFCMTokenView
+from .views import NotificationView, UpdateFCMTokenView
 
 urlpatterns = [
-    path('update-fcm-token/', UpdateFCMTokenView.as_view(), name='update-fcm-token'),
+    path("update-fcm-token/", UpdateFCMTokenView.as_view(), name="update-fcm-token"),
+    path("", NotificationView.as_view(), name="notification"),
 ]
